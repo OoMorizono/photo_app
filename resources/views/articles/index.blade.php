626ed2bb074e36ea6ee7a4372feb05129a16762f
OoMorizono <x65f588d72sw76@gmail.com>
2021-10-16 11:59:42 +0900

    create画面作成

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -16,4 +16,7 @@
     </div>
     @endforeach
 </section>
+<a href="{{ route('articles.create') }}" class="position-fixed fs-1 bottom-0 end-0" style="color:#ff00d4;">
+    <i class="fas fa-plus-circle"></i>
+</a>
 @endsection
